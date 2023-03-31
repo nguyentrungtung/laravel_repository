bdcb0288f38317a65c2f8190d1bc69a980cb277d
tungnt <tungnt.code@gmail.com>
2023-03-31 18:05:26 +0700

    git init

--- resources/views/admin/partials/menu.blade.php
+++ resources/views/admin/partials/menu.blade.php
@@ -7,14 +7,14 @@
         </div>
         <div class="pull-left info">
             <p>{{ Auth::user()->name }}</p>
-            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            <a href="#"><i class="fa fa-circle text-success"></i> </a>
         </div>
     </div>
     <!-- search form -->
     <!-- /.search form -->
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
-        <li class="header">MAIN NAVIGATION</li>
+        <li class="header"></li>
         <li><a href="{{ route('home.index') }}"><i class="fa fa-dashboard"></i> <span>@lang('menu.home')</span></a></li>
 
         <li class="treeview">
@@ -29,25 +29,12 @@
             </ul>
         </li>
 
-        <li class="treeview">
-            <a href="#">
-                <i class="fa fa-heart"></i> <span>@lang('common.mail')</span>
-                <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-                </span>
-            </a>
-            <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-hand-o-right"></i>@lang('common.mail_template')</a></li>
-            </ul>
-        </li>
+
 
 
 
         {{--<li><a href="#"><i class="fa fa-users"></i> <span>Documentation</span></a></li>--}}
-        <li class="header">LABELS</li>
-        <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+
     </ul>
 </section>
 <!-- /.sidebar -->
